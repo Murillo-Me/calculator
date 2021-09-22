a52503f7633024f611bbae1dc3f6253ac71c5ca5
Murillo Mendes <murillomendes95@hotmail.com>
2021-09-21 21:37:52 -0300

    MVP

---
+++ app.js
@@ -0,0 +1,100 @@
+function add(a,b) {
+    return a + b
+}
+
+function subtract(a,b) {
+    return a - b
+}
+
+function multiply(a,b) {
+    return a * b
+}
+
+function divide(a,b) {
+    return a / b
+}
+
+function testOperations(a,b) {
+    console.log(`Adding operation: ${a} + ${b} = ${add(a,b)}`);
+    console.log(`Subtracting operation: ${a} - ${b} = ${subtract(a,b)}`);
+    console.log(`Multiplying operation: ${a} * ${b} = ${multiply(a,b)}`);
+    console.log(`Dividing operation: ${a} / ${b} = ${divide(a,b)}`);
+}
+
+function resetDisplay() {
+    display.textContent = 0
+    operationOnMemory = null
+    receiveSecondDigit = false
+    enterFirstNumber = true
+    enterSecondNumber = false
+}
+
+function invertSign() {
+    display.textContent = -1 * parseFloat(display.textContent)
+}
+
+let operationOnMemory = null
+let receiveSecondDigit = false
+let enterFirstNumber = true
+let enterSecondNumber = false
+
+let firstNumber = null
+let secondNumber = null
+
+function saveOperator() {
+    firstNumber = parseFloat(display.textContent)
+    operationOnMemory = this.id
+    receiveSecondDigit = true
+    enterSecondNumber = true
+}
+
+function computeOperation() {
+    secondNumber = parseFloat(display.textContent)
+    switch (operationOnMemory) {
+        case "add":
+            display.textContent = add(firstNumber, secondNumber);
+            break;
+        case "subtract":
+            display.textContent = subtract(firstNumber, secondNumber);
+            break;
+        case "multiply":
+            display.textContent = multiply(firstNumber, secondNumber);
+            break;
+        case "divide":
+            display.textContent = divide(firstNumber, secondNumber);
+            break;
+    }
+    operationOnMemory = null
+    firstNumber = null
+    secondNumber = null
+    enterFirstNumber = true
+    enterSecondNumber = false
+}
+
+const display = document.querySelector('span#display')
+display.textContent = 0
+
+const resetBtn = document.querySelector('button#AC')
+resetBtn.addEventListener('click', resetDisplay)
+
+const digits = document.querySelectorAll('.digit')
+digits.forEach(digitBtn => digitBtn.addEventListener('click', e => {
+    if (display.textContent.length > 8) return
+
+    if (!receiveSecondDigit) {
+        if (display.textContent == 0) display.textContent = e.target.textContent
+        else display.textContent += e.target.textContent
+    } else {
+        receiveSecondDigit = false
+        display.textContent = e.target.textContent
+    }
+}))
+
+const signBtn = document.querySelector('button#sign')
+signBtn.addEventListener('click', invertSign)
+
+const operators = document.querySelectorAll('.operator')
+operators.forEach(operator => operator.addEventListener('click', saveOperator))
+
+const equals = document.querySelector('button#equals')
+equals.addEventListener('click', computeOperation)
