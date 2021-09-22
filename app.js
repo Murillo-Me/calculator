d409ac3cfa1099387c7386c769caaa1ccfce620f
Murillo Mendes <murillomendes95@hotmail.com>
2021-09-22 20:31:48 -0300

    Added the rest of keyboard functionality + fitting numbers to display

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+Number.prototype.countDecimals = function () {
+    if(Math.floor(this.valueOf()) === this.valueOf()) return 0;
+    return this.toString().split(".")[1].length || 0; 
+}
+
 function add(a,b) {
     return a + b
 }
@@ -11,7 +16,8 @@ function multiply(a,b) {
 }
 
 function divide(a,b) {
-    return a / b
+    if (b == 0) return 'ERROR'
+    else return a / b
 }
 
 function testOperations(a,b) {
@@ -29,6 +35,19 @@ function resetDisplay() {
     enterSecondNumber = false
 }
 
+function eraseLastDigit() {
+    if (display.textContent.length == 1) {
+        display.textContent = 0
+        return
+    }
+    display.textContent = display.textContent.slice(0,-1)
+}
+
+function makeDecimal() {
+    if (display.textContent.includes('.')) return;
+    else display.textContent += '.'
+}
+
 function invertSign() {
     display.textContent = -1 * parseFloat(display.textContent)
 }
@@ -37,8 +56,31 @@ function numberToPercentage() {
     display.textContent = parseFloat(display.textContent) / 100
 }
 
+// COMPUTES USER INPUT FROM KEYBOARD
 function keyboardInput(event) {
+    console.log(event.keyCode);
+
+    // IF USER INPUTS 'ENTER', THEN RETURN RESULT
+    if (event.keyCode == 13) {
+        computeOperation()
+        return
+    }
+
+    // IF USER INPUTS 'ESC', THEN CLEAR CALCULATOR
+    if (event.keyCode == 27) {
+        resetDisplay()
+        return
+    }
+
+    // IF USER INPUTS 'BACKSPACE' THEN CLEAR LAST DIGIT
+    if (event.keyCode == 8) {
+        eraseLastDigit()
+        return
+    }
+
+    // IF USER INPUTS A NUMBER FROM 0 TO 9, SHOW ON DISPLAY
     if (isNumericInput(event.keyCode)) {
+
         if (display.textContent.length > 8) return
 
         if (!receiveSecondDigit) {
@@ -48,13 +90,43 @@ function keyboardInput(event) {
             receiveSecondDigit = false
             display.textContent = event.key
         }
+
     } else {
+        // IF USER INPUTS AN OPERATOR, SAVE IT, THEN GET NEXT NUMBER
+        if (isOperatorInput(event.keyCode) != undefined) {
+            saveOperator(isOperatorInput(event.keyCode).operator)
+        }        
         return
     }
 }
 
+// CHECKS IF KEY PRESSED IS 0-9, BOTH ON NUMERIC ROW OF KEYBOARD AND NUM PAD
 function isNumericInput(keyCode) {
     return ((keyCode >= 48 && keyCode <= 57) || (keyCode >= 96 && keyCode <= 105))
+}
+
+function isOperatorInput(keyCode) {
+    const operatorCodes = [{
+                                key: 106,
+                                operator: 'multiply',
+                            },
+                            {
+                                key: 107,
+                                operator: 'add',
+                            },
+                            {
+                                key: 109,
+                                operator: 'subtract',
+                            },
+                            {
+                                key: 111,
+                                operator: 'divide',
+                            }
+                        ]
+
+    return (operatorCodes.find(code => {
+        return (code.key == keyCode)
+    }))
 }
 
 let operationOnMemory = null
@@ -65,29 +137,45 @@ let enterSecondNumber = false
 let firstNumber = null
 let secondNumber = null
 
-function saveOperator() {
+function saveOperator(operator) {
     firstNumber = parseFloat(display.textContent)
-    operationOnMemory = this.id
+
+    if (saveOperator.caller == keyboardInput) operationOnMemory = operator
+    else operationOnMemory = this.id
+
     receiveSecondDigit = true
     enterSecondNumber = true
 }
 
 function computeOperation() {
     secondNumber = parseFloat(display.textContent)
+    let result = null
     switch (operationOnMemory) {
         case "add":
-            display.textContent = add(firstNumber, secondNumber);
+            result = add(firstNumber, secondNumber);
             break;
         case "subtract":
-            display.textContent = subtract(firstNumber, secondNumber);
+            result = subtract(firstNumber, secondNumber);
             break;
         case "multiply":
-            display.textContent = multiply(firstNumber, secondNumber);
+            result = multiply(firstNumber, secondNumber);
             break;
         case "divide":
-            display.textContent = divide(firstNumber, secondNumber);
+            result = divide(firstNumber, secondNumber);
             break;
     }
+
+    if (result != 'ERROR' && result.countDecimals() > 2) result = Math.round(result * 100) / 100
+
+    if (result != 'ERROR' && result/10000000 > 1) {
+        result = result.toExponential()
+        if (result.length > 7) {
+            result = result.slice(0,6) + result.slice(-1*(result.length - result.indexOf('e')))
+        }
+    }
+    
+    display.textContent = result
+
     operationOnMemory = null
     firstNumber = null
     secondNumber = null
@@ -113,6 +201,9 @@ digits.forEach(digitBtn => digitBtn.addEventListener('click', e => {
         display.textContent = e.target.textContent
     }
 }))
+
+const decimalBtn = document.querySelector('button#decimal')
+decimalBtn.addEventListener('click', makeDecimal)
 
 const signBtn = document.querySelector('button#sign')
 signBtn.addEventListener('click', invertSign)
